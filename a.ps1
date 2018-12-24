f0021700ef77f3db556473ec641ae663c9b8e6ca
Victor Putrov <victor.putrov@aspose.com>
2018-12-24 22:44:44 +0300

    generate SDK sources for v18.12

--- a.ps1
+++ a.ps1
@@ -1,6 +1,11 @@
+echo "lalala1"
 git add .
+echo "lalala2"
 git config user.email "victor.putrov@aspose.com"
+echo "lalala3"
 git config user.name "Victor Putrov"
+echo "lalala4"
 git commit -m "generate SDK sources for v18.12"
+echo "lalala5"
 git push origin HEAD:master
-echo "lalala"
+echo "lalala6"
