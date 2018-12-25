1cb0c4a4074e769827c98ebe44176f53cdfad82a
Victor Putrov <victor.putrov@aspose.com>
2018-12-25 11:15:25 +0300

    generate SDK sources for v18.11

--- a.ps1
+++ a.ps1
@@ -1,15 +1,27 @@
+param([String]$v)
+
+if (-Not $v) {
+	echo "Version not specified. Run this script with -v option specified."
+	exit 1
+}
 if (git status --porcelain) {
-echo "lalala1"
-git add .
-echo "lalala2"
-git config user.email "victor.putrov@aspose.com"
-echo "lalala3"
-git config user.name "Victor Putrov"
-echo "lalala4"
-git commit -m "generate SDK sources for v18.12"
-echo "lalala5"
-git push origin HEAD:master
-echo "lalala67"
+	echo "Configuring..."
+#cd %1
+#	SET DISPLAY=localhost:0.0
+	echo %CD%\..\..\build\git_ssh.sh
+	echo SSH_ASKPASS=%CD%\..\..\build\Askpass.exe
+	echo "Adding..."
+	git add .
+	echo "Committing..."
+	git config user.email "victor.putrov@aspose.com"
+	git config user.name "Victor Putrov"
+	git commit -m "generate SDK sources for v18.11"
+	echo "Pushing..."
+	git push origin HEAD:master
+	echo "Tagging..."
+	git tag -a $v -m $v --force
+	git push --tags
+	echo "Finished."
 } else {
-	echo "dududu"
+	echo "No changes. Nothing to push."
 }
